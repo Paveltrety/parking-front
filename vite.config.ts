33c78fc2f089cd8339060d514986d596815e0baa
Pavel Tretyakov <pasha57@bk.ru>
2024-03-29 22:40:49 +0300

    T-3 feat: добавил скрипт для деплоя на гитхаб пейдж

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: "/parking-front/",
   plugins: [react()],
   server: {
     port: 8082,
